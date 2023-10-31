9e6310952cfa32e1318cff4d210c29e4a6453b6d
okarpuk <oleg.karpuk.1982@gmail.com>
2023-10-31 23:49:44 +0300

    test

--- incapsulation.py
+++ incapsulation.py
@@ -20,3 +20,26 @@ antonio.set_age(98)
 antonio.display_info()
 print(antonio.get_age())
 
+
+# Инкапсуляция через аннотации
+class Person:
+    def __init__(self, name):
+        self.__name = name
+        self.__age = 50
+
+    @property
+    def age_getter(self):
+        return self.__age
+
+    @age_getter.setter
+    def age_setter(self, age):
+        self.__age = age
+
+    def display_info(self):
+        print(f"Имя: {self.__name}, Возраст: {self.__age}")
+
+tom = Person("Tom")
+tom.age_setter = 36
+tom.display_info()
+
+
