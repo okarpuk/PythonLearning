cabf4d693abb8428bb8a9a3ce2e50f86c2a17810
okarpuk <oleg.karpuk.1982@gmail.com>
2023-10-31 19:03:13 +0300

    test

--- 01.py
+++ 01.py
@@ -1,1 +1,8 @@
-print ("1231234")
+people = ["Bob", "Jim", "Alex", "Tom", "Ivan", "Joe"]
+
+for f in people [1:4]:
+    if f == "Jim":
+        var = "Engineer " + f
+        print(var)
+    # else:
+    #     print("No engineers")
