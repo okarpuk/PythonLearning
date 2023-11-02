d9ddd16dc2c24c1decb3b55133070f234ec0eed1
okarpuk <oleg.karpuk.1982@gmail.com>
2023-11-02 23:12:41 +0300

    Test

--- 02_fields_input_and_click_button.py
+++ 02_fields_input_and_click_button.py
@@ -9,12 +9,13 @@ driver.maximize_window()
 user_name = driver.find_element(By.ID, "user-name")
 user_name.send_keys("standard_user")
 
-password = driver.find_element(By.NAME, "password")
+password = driver.find_element(By.CSS_SELECTOR, "#password")
 password.send_keys("secret_sauce")
 time.sleep(3)
 
-driver.find_element(By.XPATH, "//input[@id='login-button']").click()
-# driver.find_element(By.XPATH, "//*[@id='login-button']").click()
+button_login = driver.find_element(By.XPATH, "//input[@id='login-button']")
+# button_login = driver.find_element(By.XPATH, "//*[@id='login-button']").click()
+button_login.click()
 time.sleep(3)
 
 driver.close()
