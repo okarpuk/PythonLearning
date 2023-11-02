7270c5f4c40fcd6362ae98cdbafcdd83c0e889c3
okarpuk <oleg.karpuk.1982@gmail.com>
2023-11-02 22:46:32 +0300

    Test

--- 02_fill_fields_with_sendkeys.py
+++ 02_fill_fields_with_sendkeys.py
@@ -9,11 +9,11 @@ driver.maximize_window()
 user_name = driver.find_element(By.ID, "user-name")
 user_name.send_keys("standard_user")
 
-password = driver.find_element(By.ID, "password")
+password = driver.find_element(By.NAME, "password")
 password.send_keys("secret_sauce")
 time.sleep(3)
 
-driver.find_element(By.ID, "login-button").click()
+driver.find_element(By.XPATH, "//input[@id='login-button']").click()
 time.sleep(3)
 
 driver.close()
