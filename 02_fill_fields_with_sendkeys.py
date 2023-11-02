e9bb28e7a39a2a194da36ad89022d754f2bc414b
okarpuk <oleg.karpuk.1982@gmail.com>
2023-11-02 22:31:30 +0300

    Test

---
+++ 02_fill_fields_with_sendkeys.py
@@ -0,0 +1,20 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get('https://www.saucedemo.com/')
+driver.maximize_window()
+
+user_name = driver.find_element(By.ID, "user-name")
+user_name.send_keys("standard_user")
+
+password = driver.find_element(By.ID, "password")
+password.send_keys("secret_sauce")
+
+time.sleep(3)
+driver.close()
+
+
+
+
