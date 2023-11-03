202c4d0714e5cbd97e0aae4461caf7404e459c57
okarpuk <oleg.karpuk.1982@gmail.com>
2023-11-03 22:16:39 +0300

    Test

---
+++ 03_first_test.py
@@ -0,0 +1,33 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+base_url = 'https://www.saucedemo.com/'
+driver.get(base_url)
+driver.maximize_window()
+
+login_standard_user = "standard_user"
+user_name = driver.find_element(By.XPATH, "//input[@id='user-name']")
+user_name.send_keys(login_standard_user)
+
+password_standard_user = "secret_sauce"
+password = driver.find_element(By.XPATH, "//input[@id='password']")
+password.send_keys(password_standard_user)
+time.sleep(3)
+
+button_login = driver.find_element(By.XPATH, "//input[@id='login-button']")
+button_login.click()
+time.sleep(3)
+
+products_page_locator = driver.find_element(By.XPATH, "//span[@class='title']")
+value_text_products = products_page_locator.text
+print(value_text_products)
+
+assert value_text_products == "Products"
+
+
+
+
+
+driver.close()
