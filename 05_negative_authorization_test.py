e5628570e17a099f7c4152923b0627f2e8313188
okarpuk <oleg.karpuk.1982@gmail.com>
2023-11-04 00:31:02 +0300

    Test

---
+++ 05_negative_authorization_test.py
@@ -0,0 +1,30 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+base_url = 'https://www.saucedemo.com/'
+driver.get(base_url)
+driver.maximize_window()
+
+login_standard_user = "standard_user"
+user_name = driver.find_element(By.XPATH, "//input[@id='user-name']")
+user_name.send_keys(login_standard_user)
+
+password_standard_user = "secret_sauce1"
+password = driver.find_element(By.XPATH, "//input[@id='password']")
+password.send_keys(password_standard_user)
+time.sleep(3)
+
+button_login = driver.find_element(By.XPATH, "//input[@id='login-button']")
+button_login.click()
+time.sleep(3)
+
+invalid_login_error = driver.find_element(By.XPATH, "//h3[@data-test='error']")
+invalid_login_error_message = invalid_login_error.text
+
+assert invalid_login_error_message == "Epic sadface: Username and password do not match any user in this service"
+
+print("Test passed")
+
+driver.close()
