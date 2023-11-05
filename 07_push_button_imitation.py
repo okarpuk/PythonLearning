5012261646dfee9b38b5e1ec975cece0752e9373
okarpuk <oleg.karpuk.1982@gmail.com>
2023-11-05 19:38:04 +0300

    Test

--- 07_push_button_imitation.py
+++ 07_push_button_imitation.py
@@ -11,18 +11,19 @@ driver.maximize_window()
 login_standard_user = "standard_user"
 user_name = driver.find_element(By.XPATH, "//input[@id='user-name']")
 user_name.send_keys(login_standard_user)
+# user_name.send_keys(Keys.BACKSPACE)   # имитация нажатия BACKSPACE
 
 password_standard_user = "secret_sauce"
 password = driver.find_element(By.XPATH, "//input[@id='password']")
 password.send_keys(password_standard_user)
-password.send_keys(Keys.RETURN) # имитация нажатия Enter
+password.send_keys(Keys.RETURN)     # имитация нажатия ENTER
 
 # button_login = driver.find_element(By.XPATH, "//input[@id='login-button']")
 # button_login.click()
 
 dropdown_list = driver.find_element(By.XPATH, "//select[@class='product_sort_container']")
 dropdown_list.click()
-dropdown_list.send_keys(Keys.ARROW_DOWN) # имитация нажатия стрелки Вниз
+dropdown_list.send_keys(Keys.ARROW_DOWN)    # имитация нажатия СТРЕЛКИ ВНИЗ
 dropdown_list.send_keys(Keys.RETURN)
 
 time.sleep(3)
