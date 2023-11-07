0c3f923f6f3e63595206055a3946447187c2e98c
okarpuk <oleg.karpuk.1982@gmail.com>
2023-11-07 21:43:07 +0300

    Test

--- 15_Checkbox.py
+++ 15_Checkbox.py
@@ -8,22 +8,28 @@ driver.get(base_url)
 driver.maximize_window()
 
 login_standard_user = "standard_user"
-elements = driver.find_element(By.XPATH, "//*[@id='app']/div/div/div[2]/div/div[1]")
-elements.click()
+elements_button = driver.find_element(By.XPATH, "//*[@id='app']/div/div/div[2]/div/div[1]")
+elements_button.click()
 time.sleep(2)
 
 checkbox_button = driver.find_element(By.XPATH, "//li[@id='item-1']")
 checkbox_button.click()
 time.sleep(2)
 
+checkbox_tree = driver.find_element(By.XPATH, "//button[@class='rct-collapse rct-collapse-btn']")
+checkbox_tree.click()
 
-
-# elements.send_keys(login_standard_user)
-
-
-
-
-
+checkbox = driver.find_element(By.XPATH, "//*[@id='tree-node']/ol/li/span/label/span[1]")
+checkbox.click()
 time.sleep(2)
 
+# Assertion is checkbox marked
+is_checked = checkbox.is_selected()
+
+if is_checked:
+    print("Checkbox marked")
+else:
+    print("Checkbox does not marked")
+
+time.sleep(2)
 driver.close()
