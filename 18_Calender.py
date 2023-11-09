a670cc55fef6bc0c4164f0ac7c5e69cce7c8bcec
okarpuk <oleg.karpuk.1982@gmail.com>
2023-11-09 09:14:11 +0300

    111

--- 18_Calender.py
+++ 18_Calender.py
@@ -8,25 +8,24 @@ base_url = 'https://demoqa.com/date-picker/'
 driver.get(base_url)
 driver.maximize_window()
 
-action = ActionChains(driver)
-
 date_field = driver.find_element(By.XPATH, "//input[@id='datePickerMonthYearInput']")
-#date_field.clear() # Should be this Method, but it doesn't work. So, we create "kostyl"- using BACKSPACE * 10
-date_field.send_keys(Keys.BACKSPACE) #1
-date_field.send_keys(Keys.BACKSPACE) #2
-date_field.send_keys(Keys.BACKSPACE) #3
-date_field.send_keys(Keys.BACKSPACE) #4
-date_field.send_keys(Keys.BACKSPACE) #5
-date_field.send_keys(Keys.BACKSPACE) #6
-date_field.send_keys(Keys.BACKSPACE) #7
-date_field.send_keys(Keys.BACKSPACE) #8
-date_field.send_keys(Keys.BACKSPACE) #9
-date_field.send_keys(Keys.BACKSPACE) #10
+#date_field.clear() # Оптимально использовать этот  метод. Но если он не работает, используем костыли
+
+for i in range(len(date_field.get_attribute(name="value"))): # с помощью цилка получаем длину текста в поле
+    date_field.send_keys(Keys.BACKSPACE)                     # и вводим BACKSPACE по количеству символов
+
+# date_field.send_keys(Keys.BACKSPACE * 10) # либо просто вводим BACKSPACE по количеству символов
 time.sleep(2)
 
 date_field.send_keys("10/10/2023")
 time.sleep(2)
 date_field.send_keys(Keys.RETURN)
+
+driver.refresh() # refresh page
+
+
+
+
 
 
 
