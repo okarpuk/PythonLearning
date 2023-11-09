34099309373a49011c005c3e8400e171b8a8cfaf
okarpuk <oleg.karpuk.1982@gmail.com>
2023-11-09 22:29:05 +0300

    Test

---
+++ L19_HW_4_23_DatePicker.py
@@ -0,0 +1,31 @@
+import time
+import datetime
+from selenium import webdriver
+from selenium.webdriver import ActionChains, Keys
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+base_url = 'https://demoqa.com/date-picker/'
+driver.get(base_url)
+driver.maximize_window()
+
+date_field = driver.find_element(By.XPATH, "//input[@id='datePickerMonthYearInput']")
+
+# Get text lenght in date field and use Backspace for each symbol
+for i in range(len(date_field.get_attribute(name="value"))):
+    date_field.send_keys(Keys.BACKSPACE)
+
+# Automatic date generation
+date_field.click()
+test_day = int(datetime.datetime.utcnow().strftime("%d")) + 10
+print(f"Test day - {test_day}")
+test_month = datetime.datetime.utcnow().strftime("%m")
+print(f"Test month - {test_month}")
+test_year = datetime.datetime.utcnow().strftime("%Y")
+print(f"Test year - {test_year}")
+
+# Send generated date into field
+date_field.send_keys(f"{test_month}/{test_day}/{test_year}")
+date_field.send_keys(Keys.RETURN)
+time.sleep(2)
+
