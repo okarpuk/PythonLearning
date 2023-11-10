8da2e7cc56839dd365bfc5d2603c5a1fd8bfd999
okarpuk <oleg.karpuk.1982@gmail.com>
2023-11-10 16:35:59 +0300

    111

---
+++ L21_Exceptions.py
@@ -0,0 +1,20 @@
+import time
+from selenium import webdriver
+from selenium.common import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+base_url = 'https://demoqa.com/dynamic-properties/'
+driver.get(base_url)
+driver.maximize_window()
+
+try:
+    visible_after = driver.find_element(By.XPATH, "//button[@id='visibleAfter']")
+    visible_after.click()
+except NoSuchElementException as exception:
+    time.sleep(10)
+    visible_after.click()
+    print("[Visible after] button clicked")
+
+time.sleep(5)
+driver.close()
