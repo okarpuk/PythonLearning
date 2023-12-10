a16fe6148cd4d902227aa5009ab667b06ba069f3
okarpuk <oleg.karpuk.1982@gmail.com>
2023-12-10 15:40:49 +0300

    111

--- Final_Projects.py
+++ Final_Projects.py
@@ -1,13 +1,16 @@
 import time
 from selenium import webdriver
-from selenium.webdriver import Keys
+from selenium.webdriver import Keys, ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
 base_url = 'https://www.7745.by/'
 driver.get(base_url)
 driver.maximize_window()
+
 
 ######### MAIN PAGE ###########
 
@@ -33,12 +36,23 @@ user_password.send_keys(password_standard_user)
 
 enter_2_button = driver.find_element(By.CSS_SELECTOR,"div[class='open-logon open-login'] div input[value='Войти']")
 enter_2_button.click()
-print("Enter 2 button clicked")
-time.sleep(10)
+# time.sleep(10)
 
 TV_button = driver.find_element(By.XPATH,"//a[@class='header-categories__item'][contains(text(),'Телевизоры')]")
 TV_button.click()
-time.sleep(5)
+
 
 ######### TV PAGE ###########
+
+action = ActionChains(driver)
+price_slider_1 = driver.find_element(By.XPATH, "//*[@id='filter-range-price']/span[1]")
+action.click_and_hold(price_slider_1).move_by_offset(30, 0).release().perform()
+
+price_slider_2 = driver.find_element(By.XPATH, "//*[@id='filter-range-price']/span[2]")
+action.click_and_hold(price_slider_2).move_by_offset(-100, 0).release().perform()
+
+time.sleep(5)
+driver.close()
+
+
 
