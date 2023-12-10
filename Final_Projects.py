fb89d298b83635641736cb0ce954bbe6dd994df9
okarpuk <oleg.karpuk.1982@gmail.com>
2023-12-10 15:18:35 +0300

    111

--- Final_Projects.py
+++ Final_Projects.py
@@ -9,20 +9,19 @@ base_url = 'https://www.7745.by/'
 driver.get(base_url)
 driver.maximize_window()
 
+######### MAIN PAGE ###########
+
 enter_button = driver.find_element(By.XPATH,"//div[@id='logon-link']/div[2]")
 enter_button.click()
-time.sleep(2)
 
 selector_registration = driver.find_element(By.XPATH,"//div[@class='open-logon open-login']//div//select[@name='prefix']")
 selector_registration.click()
-time.sleep(2)
 
 se = Select(selector_registration)
 for item in se.options:
     if item.text == 'e-mail':
         item.click()
         break
-time.sleep(2)
 
 login_standard_user = "final_project_test@mail.ru"
 user_name = driver.find_element(By.XPATH, "//div[@class='open-logon open-login']//div//input[@id='login-modal-input-login']")
@@ -31,11 +30,15 @@ user_name.send_keys(login_standard_user)
 password_standard_user = "1A.2b.3c.4d"
 user_password = driver.find_element(By.XPATH, "//div[@class='open-logon open-login']//div//input[@id='password']")
 user_password.send_keys(password_standard_user)
-time.sleep(2)
 
-enter_2_button = driver.find_element(By.XPATH,"//div[@class='modal-layout login-modal']")
+enter_2_button = driver.find_element(By.CSS_SELECTOR,"div[class='open-logon open-login'] div input[value='Войти']")
 enter_2_button.click()
-time.sleep(2)
+print("Enter 2 button clicked")
+time.sleep(10)
 
+TV_button = driver.find_element(By.XPATH,"//a[@class='header-categories__item'][contains(text(),'Телевизоры')]")
+TV_button.click()
+time.sleep(5)
 
+######### TV PAGE ###########
 
